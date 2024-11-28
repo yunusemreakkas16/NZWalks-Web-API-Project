07de29a4cb0b06cac5138efd1d444b9120a8a4f2
Yunus Emre Akkaş <58690489+yunusemreakkas16@users.noreply.github.com>
2024-11-28 18:09:26 +0300

    Has been added custom controller
    A custom controller and an action were added to the project, and their functionality was successfully tested using Swagger

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // http://localhost:5185/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "Jonathan", "William", "Winston", "Mark" };
+            return Ok(studentNames);
+        }
+    }
+}
