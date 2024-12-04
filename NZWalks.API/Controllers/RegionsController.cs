a806548afab79f93de3b20256627885110d6f9f5
Yunus Emre Akkaş <58690489+yunusemreakkas16@users.noreply.github.com>
2024-12-04 19:34:52 +0300

    Post request and Create method has been added

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -67,5 +67,34 @@ namespace NZWalks.API.Controllers
             };
             return Ok(regionDto);
         }
+
+        // POST To Create New Region
+        // POST: https://localhost:7280/api/regions
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            //Map or Convert DTO to Domain Model
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl=addRegionRequestDto.RegionImageUrl,
+            };
+
+            //Use Domain Model to create Region
+            dbContext.Regions.Add(regionDomainModel);
+            dbContext.SaveChanges();
+
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl,
+            };
+
+            return CreatedAtAction(nameof(GetById), new {id = regionDto.Id }, regionDto);
+
+        }
     }
 }
