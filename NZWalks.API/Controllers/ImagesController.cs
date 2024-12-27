4bd393df9b771b24b9d27e0214794e343f9a4c73
Yunus Emre Akkaş <58690489+yunusemreakkas16@users.noreply.github.com>
2024-12-27 16:00:19 +0300

    Adding and making Custom validation method for Image Uploading

---
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        // POST: /api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto requestDto)
+        {
+            ValidateFileUpload(requestDto);
+
+            if(ModelState.IsValid)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+        
+        //Validation
+        private void ValidateFileUpload (ImageUploadRequestDto requestDto)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png", ".jxr" };
+
+            if (!allowedExtensions.Contains(Path.GetExtension(requestDto.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsupported file extension");
+            }
+
+            if (requestDto.File.Length > 52428800)
+            {
+                ModelState.AddModelError("file", "File size more than 50MB, please upload smaller size file");
+            }
+        }
+    }
+}
--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae418ac488f7183942cd886ced1bba52d98dd6e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53cbd2deaad0130aa5e8e6a927718bf45c754649")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
